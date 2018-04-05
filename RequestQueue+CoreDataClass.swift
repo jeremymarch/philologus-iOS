56d63aff8b0a722122014c7ab8274c1820f20633
jeremymarch <jmarch@gradcenter.cuny.edu>
2018-04-05 00:29:12 -0400

    Added ManagedObject classes for RequestQueue

---
+++ RequestQueue+CoreDataClass.swift
@@ -0,0 +1,17 @@
+//
+//  RequestQueue+CoreDataClass.swift
+//  philologus
+//
+//  Created by Jeremy March on 4/5/18.
+//  Copyright © 2018 Jeremy March. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+//in case of error when creating
+//https://stackoverflow.com/questions/40410169/invalid-redeclaration-on-coredata-classes
+public class RequestQueue: NSManagedObject {
+
+}
---
+++ RequestQueue+CoreDataProperties.swift
@@ -0,0 +1,22 @@
+//
+//  RequestQueue+CoreDataProperties.swift
+//  philologus
+//
+//  Created by Jeremy March on 4/5/18.
+//  Copyright © 2018 Jeremy March. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension RequestQueue {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<RequestQueue> {
+        return NSFetchRequest<RequestQueue>(entityName: "RequestQueue")
+    }
+
+    @NSManaged public var data: String?
+
+}
